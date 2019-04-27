5173b98c1cc6ce5151011aa27fc720613a3a73b0
Dzmitry Ivanou <3prn@mail.ru>
2019-04-27 16:03:15 +0300

    feat: imlement the Transform tool (clicking on a figure will change square for circle)

--- pallete/index.js
+++ pallete/index.js
@@ -1,6 +1,6 @@
 const paintBacket = document.getElementById('paint-backet');
-const chooseColor = document.getElementById('choose-color'); 
-const move =  document.getElementById('move');
+const chooseColor = document.getElementById('choose-color');
+const move = document.getElementById('move');
 const transform = document.getElementById('transform');
 const state = {
     correntTool: '',
@@ -9,7 +9,7 @@ paintBacket.addEventListener("click", function (event) {
     chooseColor.classList.remove('button-active');
     move.classList.remove('button-active');
     transform.classList.remove('button-active');
-    if (state.correntTool === 'paintBacket')  {
+    if (state.correntTool === 'paintBacket') {
         paintBacket.classList.remove('button-active');
         state.correntTool = '';
     }
@@ -74,52 +74,133 @@ canvasItem1.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem1.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
     }
+    if (state.correntTool === 'transform') {
+        canvasItem1.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem1.classList.add('circle');
+        }
+        else {
+            canvasItem1.classList.remove('circle');
+        }
+    }
 })
 canvasItem2.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem2.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+    }
+    if (state.correntTool === 'transform') {
+        canvasItem2.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem2.classList.add('circle');
+        }
+        else {
+            canvasItem2.classList.remove('circle');
+        }
     }
 })
 canvasItem3.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem3.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
     }
+    if (state.correntTool === 'transform') {
+        canvasItem3.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem3.classList.add('circle');
+        }
+        else {
+            canvasItem3.classList.remove('circle');
+        }
+    }
 })
 canvasItem4.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem4.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+    }
+    if (state.correntTool === 'transform') {
+        canvasItem4.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem4.classList.add('circle');
+        }
+        else {
+            canvasItem4.classList.remove('circle');
+        }
     }
 })
 canvasItem5.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem5.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
     }
+    if (state.correntTool === 'transform') {
+        canvasItem5.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem5.classList.add('circle');
+        }
+        else {
+            canvasItem5.classList.remove('circle');
+        }
+    }
 })
 canvasItem6.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem6.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+    }
+    if (state.correntTool === 'transform') {
+        canvasItem6.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem6.classList.add('circle');
+        }
+        else {
+            canvasItem6.classList.remove('circle');
+        }
     }
 })
 canvasItem7.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem7.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
     }
+    if (state.correntTool === 'transform') {
+        canvasItem7.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem7.classList.add('circle');
+        }
+        else {
+            canvasItem7.classList.remove('circle');
+        }
+    }
 })
 canvasItem8.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem8.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+    }
+    if (state.correntTool === 'transform') {
+        canvasItem8.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem8.classList.add('circle');
+        }
+        else {
+            canvasItem8.classList.remove('circle');
+        }
     }
 })
 canvasItem9.addEventListener("click", (event) => {
     if (state.correntTool === 'paintBacket') {
         canvasItem9.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
     }
+    if (state.correntTool === 'transform') {
+        canvasItem9.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
+        if (event.target.className !== 'circle') {
+            canvasItem9.classList.add('circle');
+        }
+        else {
+            canvasItem9.classList.remove('circle');
+        }
+    }
 })
 
 document.addEventListener("click", (event) => {
-    if(state.correntTool === 'chooseColor' && event.target.parentNode.id !== "choose-color" && event.target.id !== "choose-color") {
+    if (state.correntTool === 'chooseColor' && event.target.parentNode.id !== "choose-color" && event.target.id !== "choose-color") {
         const newColor = window.getComputedStyle(event.target).backgroundColor;
-        prevColor.style.backgroundColor =  window.getComputedStyle(currentColor).backgroundColor;
+        prevColor.style.backgroundColor = window.getComputedStyle(currentColor).backgroundColor;
         currentColor.style.backgroundColor = newColor;
     }
 })
